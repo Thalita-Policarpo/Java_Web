1babd435e13def676591373e4f9ada6712cb92ca
Thalita Policarpo <thalita_policarpo@outlook.com>
2022-12-08 13:03:37 -0300

    criação das classes filhas

--- appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/domain/Programacao.java
+++ appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/domain/Programacao.java
@@ -2,4 +2,16 @@ package br.edu.infnet.appcurso.model.domain;
 
 public class Programacao extends Curso {
 
+	
+	public Programacao(String nome, float valor, int codigo) {
+		super(nome, valor, codigo);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String retornarMensagem() {
+		
+		return "";
+	}
+	
 }
