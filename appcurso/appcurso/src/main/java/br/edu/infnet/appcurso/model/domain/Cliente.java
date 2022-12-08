73b9536646bf629875c63a6264aac193163300a4
Thalita Policarpo <thalita_policarpo@outlook.com>
2022-12-08 13:02:44 -0300

    Criação da classe de associação

--- appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/domain/Cliente.java
+++ appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/domain/Cliente.java
@@ -1,9 +1,45 @@
 package br.edu.infnet.appcurso.model.domain;
 
 public class Cliente {
-	private String primeiroNome;
-	private StringBuilder sobrenome;
-	private String ultimoNome;
+	private String nome;
 	private String cpf;
 	private int idade;
+	
+	
+	
+	public Cliente(String nome, String cpf, int idade) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.idade = idade;
+	}
+
+
+	@Override
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(nome);
+		sb.append(" ; ");
+		sb.append(cpf);
+		sb.append(" ; ");
+		sb.append(idade);
+		
+
+		return sb.toString();
+	}
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+		
 }
