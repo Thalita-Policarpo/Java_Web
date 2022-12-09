4eeb32876e4aded267feeeddeee87e8e304946b3
Thalita Policarpo <thalita_policarpo@outlook.com>
2022-12-08 23:13:56 -0300

    implementando classe filha

--- appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/domain/Programacao.java
+++ appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/domain/Programacao.java
@@ -2,19 +2,68 @@ package br.edu.infnet.appcurso.model.domain;
 
 public class Programacao extends Curso {
 	private String linguagem;
-	private String ;
-	private String ;
+	private String instituicao;
+	private String bibliografia;
 	
 	
-	public Programacao(String nome, float valor, int codigo) {
-		super(nome, valor, codigo);
-		// TODO Auto-generated constructor stub
+	public Programacao(String nomeCurso, float valor, int codigo, String linguagem, String instituição,
+			String bibliografia) {
+		super(nomeCurso, valor, codigo);
+		this.linguagem = linguagem;
+		this.instituicao = instituição;
+		this.bibliografia = bibliografia;
 	}
+
 
 	@Override
 	public String retornarMensagem() {
 		
-		return "";
+		return "O curso "  + getNomeCurso() + ", da instituicao " + getInstituicao() + " foi cadastrado!";
 	}
 	
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(super.toString());
+		sb.append(" ; ");
+		sb.append(linguagem);
+		sb.append(" ; ");
+		sb.append(instituicao);
+		sb.append(" ; ");
+		sb.append(bibliografia);
+		
+		return sb.toString();
+	}
+
+
+	public String getLinguagem() {
+		return linguagem;
+	}
+
+
+	public void setLinguagem(String linguagem) {
+		this.linguagem = linguagem;
+	}
+
+
+	public String getInstituicao() {
+		return instituicao;
+	}
+
+
+	public void setInstituicao(String instituição) {
+		this.instituicao = instituição;
+	}
+
+
+	public String getBibliografia() {
+		return bibliografia;
+	}
+
+
+	public void setBibliografia(String bibliografia) {
+		this.bibliografia = bibliografia;
+	}	
+
 }
