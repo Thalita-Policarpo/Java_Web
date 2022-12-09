5fd17595265d3212b6f72ad263c009860b784ed9
Thalita Policarpo <thalita_policarpo@outlook.com>
2022-12-08 22:56:28 -0300

    teste da classe filha

--- appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/test/AgileTeste.java
+++ appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/test/AgileTeste.java
@@ -1,5 +1,18 @@
 package br.edu.infnet.appcurso.model.test;
 
-public class AgileTeste {
+import br.edu.infnet.appcurso.model.domain.Agile;
 
+public class AgileTeste {
+	
+	public static void main(String[] args) {
+		
+		Agile a1 = new Agile("Metodologia agil", 70, 1, "Kamban", "1 mes", false);
+		System.out.println(a1);
+		
+		Agile a2 = new Agile("Programacao", 70, 1, "Java", "6 meses", true);
+		System.out.println(a2);
+		
+		Agile a3 = new Agile("Banco de Dados", 70, 1, "JSON", "3 meses", true);
+		System.out.println(a3);
+	}
 }
