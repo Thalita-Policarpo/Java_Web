be29b3ce2170f49143fc4c15347463183e5bb0e0
Thalita Policarpo <thalita_policarpo@outlook.com>
2022-12-08 22:41:10 -0300

    implementando a classe filha

--- appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/domain/Programacao.java
+++ appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/domain/Programacao.java
@@ -1,7 +1,10 @@
 package br.edu.infnet.appcurso.model.domain;
 
 public class Programacao extends Curso {
-
+	private String linguagem;
+	private String ;
+	private String ;
+	
 	
 	public Programacao(String nome, float valor, int codigo) {
 		super(nome, valor, codigo);
