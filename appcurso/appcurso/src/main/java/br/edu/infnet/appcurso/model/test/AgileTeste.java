88a8ebe765376ce8a86150363b0a9070d005f0ac
Thalita Policarpo <thalita_policarpo@outlook.com>
2022-12-10 15:00:19 -0300

    testando classes com exceções

--- appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/test/AgileTeste.java
+++ appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/test/AgileTeste.java
@@ -1,18 +1,43 @@
 package br.edu.infnet.appcurso.model.test;
 
 import br.edu.infnet.appcurso.model.domain.Agile;
+import br.edu.infnet.appcurso.model.exceptions.ValorZeradoException;
 
 public class AgileTeste {
 	
 	public static void main(String[] args) {
 		
-		Agile a1 = new Agile("Metodologia agil", 70, 1, "Kamban", "1 mes", false);
-		System.out.println(a1);
+		try {
+			Agile a1 = new Agile("Metodologia agil", 70, 1, "Kamban", "1 mes", false);
+			System.out.println(a1);
+		} catch (ValorZeradoException e) {
+			System.out.println("[ERRO] " + e.getMessage());
+		}
 		
-		Agile a2 = new Agile("Programacao", 70, 1, "Java", "6 meses", true);
-		System.out.println(a2);
 		
-		Agile a3 = new Agile("Banco de Dados", 70, 1, "JSON", "3 meses", true);
-		System.out.println(a3);
-	}
+		try {	
+			Agile a2 = new Agile("Metodologia agil", 100, 2, "XP", "3 meses", true);
+			System.out.println(a2);
+		} catch (ValorZeradoException e) {
+			System.out.println("[ERRO] " + e.getMessage());
+		}
+	
+	
+		try {
+			Agile a3 = new Agile("Metodologia agil", -90, 3, "Kamban", "1 meses", true);
+			System.out.println(a3);
+		} catch (ValorZeradoException e) {
+			System.out.println("[ERRO] " + e.getMessage());
+		}
+		
+		
+		try {
+			Agile a4 = new Agile("Metodologia agil", 0, 4, "XP", "3 meses", true);
+			System.out.println(a4);
+		} catch (ValorZeradoException e) {
+			System.out.println("[ERRO] " + e.getMessage());
+		}
+
+}
+
 }
