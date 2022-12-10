f12e043cc3055373393f2124c16dc1a404e3b052
Thalita Policarpo <thalita_policarpo@outlook.com>
2022-12-10 17:34:06 -0300

    criação exceção de dados

---
+++ appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/exceptions/DadosPessoaisNaoPreenchidosException.java
@@ -0,0 +1,14 @@
+package br.edu.infnet.appcurso.model.exceptions;
+
+public class DadosPessoaisNaoPreenchidosException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DadosPessoaisNaoPreenchidosException(String mensagem) {
+		super(mensagem);
+	}
+	
+}
