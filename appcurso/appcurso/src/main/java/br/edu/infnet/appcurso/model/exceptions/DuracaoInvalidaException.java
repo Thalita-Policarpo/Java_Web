a3b336954cf9a1986f44f5106ada53a120fde517
Thalita Policarpo <thalita_policarpo@outlook.com>
2022-12-10 14:59:44 -0300

    criando classes de exceções

---
+++ appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/exceptions/DuracaoInvalidaException.java
@@ -0,0 +1,14 @@
+package br.edu.infnet.appcurso.model.exceptions;
+
+public class DuracaoInvalidaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DuracaoInvalidaException(String mensagem) {
+		super(mensagem);
+	}
+	
+}
---
+++ appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/exceptions/ValorZeradoException.java
@@ -0,0 +1,15 @@
+package br.edu.infnet.appcurso.model.exceptions;
+
+public class ValorZeradoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ValorZeradoException(String mensagem) {
+		super(mensagem);
+	}
+	
+	
+}
