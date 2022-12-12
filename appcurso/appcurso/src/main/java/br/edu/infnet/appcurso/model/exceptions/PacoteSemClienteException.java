c045bb34a461171f450acd73b5673f2053bb8d8f
Thalita Policarpo <thalita_policarpo@outlook.com>
2022-12-12 18:52:31 -0300

    exceções

---
+++ appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/model/exceptions/PacoteSemClienteException.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appcurso.model.exceptions;
+
+public class PacoteSemClienteException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PacoteSemClienteException(String mensagem) {
+		super(mensagem);
+	}
+}
