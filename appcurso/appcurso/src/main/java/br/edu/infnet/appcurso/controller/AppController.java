6a1d1e51233d27503b82f3410cb977fce9083d6e
Thalita Policarpo <thalita_policarpo@outlook.com>
2023-02-16 01:52:12 -0300

    utilizando model

--- appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/controller/AppController.java
+++ appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/controller/AppController.java
@@ -16,4 +16,5 @@ public class AppController {
 		return "home";
 	}
 
+
 }
--- appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/controller/UsuarioController.java
+++ appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/controller/UsuarioController.java
@@ -1,8 +1,7 @@
 package br.edu.infnet.appcurso.controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -12,15 +11,24 @@ import br.edu.infnet.appcurso.repository.UsuarioRepository;
 @Controller
 public class UsuarioController {
 
+	private String msg = null;
+
 	@GetMapping(value = "/usuario")
 	public String telaCadastro() {
 		return "usuario/cadastro";
 	}
 
 	@GetMapping(value = "/lista")
-	public String telaLista() {
-		return "lista/lista";
+	public String telaLista(Model model) {
+
+		model.addAttribute("usuarios", UsuarioRepository.obterLista());
+
+		model.addAttribute("mensagem", msg);
+		msg = null;
+
+		return "usuario/lista";
 	}
+
 
 
 	@PostMapping(value = "/usuario/incluir")
@@ -28,13 +36,15 @@ public class UsuarioController {
 
 		UsuarioRepository.incluir(usuario);
 
-		List<Usuario> lista = UsuarioRepository.obterLista();
+		msg = "A inclusão do usuário " + usuario.getNome() + " foi realizada com sucesso!";
 
-		System.out.println("Quantidade de uarios:" + lista.size());
-
-		for (Usuario user : lista) {
-			System.out.println("A inclusao do usuario " + user.getNome() + ", foi realizada com sucesso!");
-		}
+//		Collection<Usuario> lista = UsuarioRepository.obterLista();
+//
+//		System.out.println("Quantidade de uarios:" + lista.size());
+//
+//		for (Usuario user : lista) {
+//			System.out.println("A inclusao do usuario " + user.getNome() + ", foi realizada com sucesso!");
+//		}
 
 		return "redirect:/lista";
 	}
