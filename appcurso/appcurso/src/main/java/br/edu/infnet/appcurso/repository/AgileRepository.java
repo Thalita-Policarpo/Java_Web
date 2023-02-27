90e536b1352e75693b734179dd302077b23f3725
Thalita Policarpo <thalita_policarpo@outlook.com>
2023-02-26 21:53:31 -0300

    criação agile repository

---
+++ appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/repository/AgileRepository.java
@@ -0,0 +1,34 @@
+package br.edu.infnet.appcurso.repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import br.edu.infnet.appcurso.model.domain.Agile;
+
+public class AgileRepository {
+	private static Integer id = 1;
+
+	private static Map<Integer, Agile> mapaAgile = new HashMap<Integer, Agile>();
+
+	public static boolean incluir(Agile agile) {
+
+		agile.setId(id++);
+
+		try {
+			mapaAgile.put(agile.getId(), agile);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public static Agile excluir(Integer key) {
+
+		return mapaAgile.remove(key);
+	}
+
+	public static Collection<Agile> obterLista() {
+		return mapaAgile.values();
+	}
+}
