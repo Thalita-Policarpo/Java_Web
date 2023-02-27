d34c5ef4e15847d565a70c3e77f380adbf284b1b
Thalita Policarpo <thalita_policarpo@outlook.com>
2023-02-26 21:53:44 -0300

    criação do banco de dados repository

---
+++ appcurso/appcurso/src/main/java/br/edu/infnet/appcurso/repository/BancoDeDadosRepository.java
@@ -0,0 +1,34 @@
+package br.edu.infnet.appcurso.repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import br.edu.infnet.appcurso.model.domain.BancoDeDados;
+
+public class BancoDeDadosRepository {
+	private static Integer id = 1;
+
+	private static Map<Integer, BancoDeDados> mapaBancoDeDados = new HashMap<Integer, BancoDeDados>();
+
+	public static boolean incluir(BancoDeDados bancoDeDados) {
+
+		bancoDeDados.setId(id++);
+
+		try {
+			mapaBancoDeDados.put(bancoDeDados.getId(), bancoDeDados);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public static BancoDeDados excluir(Integer key) {
+
+		return mapaBancoDeDados.remove(key);
+	}
+
+	public static Collection<BancoDeDados> obterLista() {
+		return mapaBancoDeDados.values();
+	}
+}
